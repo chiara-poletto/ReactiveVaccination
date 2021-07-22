341e5e10c5ff5cad14fce2b94f8533149567b496
chiara-poletto <polettoc@gmail.com>
2021-07-22 08:23:14 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
     Simulation of an ABM for COVID-19
     @file main.cpp
     @author Pierre-Yves Boelle, Chiara Poletto
-    @acknowledgment Jesus Moreno for first version
+    @acknowledgment Jésus A Moreno López for first version fo the code
     @version 1.0 2020-04-16
     @license GPL-3.0-or-later
 */
